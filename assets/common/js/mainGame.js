9e8a50fe12f1a23a1902bb6061718931c9951389
Zebiano <sebas1@hotmail.de>
2017-12-18 12:30:43 +0000

    Updated mainGame.js
    Criei Meshes

---
+++ assets/common/js/mainGame.js
@@ -0,0 +1,48 @@
+// Areas de clique
+ctx.beginPath();
+
+// Baliza
+// Toda
+ctx.beginPath();
+ctx.fillStyle = "black";
+ctx.fillRect(357, 81, 562, 287);
+
+// Quadradoes individuais
+ctx.fillStyle = "red";
+ctx.fillRect(357, 81, 189, 97); // Quadrado 1
+ctx.fillStyle = "blue";
+ctx.fillRect(357, 176, 189, 97); // Quadrado 4
+ctx.fillStyle = "yellow";
+ctx.fillRect(357, 271, 189, 97); // Quadrado 7
+ctx.fillStyle = "yellow";
+ctx.fillRect(544, 81, 189, 97); // Quadrado 2
+ctx.fillStyle = "red";
+ctx.fillRect(544, 176, 189, 97); // Quadrado 5
+ctx.fillStyle = "blue";
+ctx.fillRect(544, 271, 189, 97); // Quadrado 8
+ctx.fillStyle = "red";
+ctx.fillRect(731, 81, 189, 97); // Quadrado 3
+ctx.fillStyle = "blue";
+ctx.fillRect(731, 176, 189, 97); // Quadrado 6
+ctx.fillStyle = "yellow";
+ctx.fillRect(731, 271, 189, 97); // Quadrado 9
+
+// Bola: X:639 Y:609
+ctx.fillStyle = "black";
+ctx.arc(639, 609, 33, 0, Math.PI * 2);
+ctx.fill();
+
+// Plateia
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, 1280, 193);
+
+// Resto do Campo
+
+/*
+
+
+// Plateia
+
+// Resto do campo
+
+*/
