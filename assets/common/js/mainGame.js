6ea24101c8c38d413977d1cfa49d6495baddc827
HugoBar <33893943+HugoBar@users.noreply.github.com>
2017-12-19 10:29:32 +0000

    adicionei a publicidade

--- assets/common/js/mainGame.js
+++ assets/common/js/mainGame.js
@@ -19,6 +19,32 @@ function Quadrado(x, y, w, h) {
     this.selected = false;
 }
 
+// PUBLICIDADE
+var imagem = new Image() // Criar variavel imagem
+imagem.src = '../assets/common/img/placares.png'; // Atribuir imagem a variavel
+
+// Canto superior esquerdo - onde vamos desenhar a imagem
+var x = 0;
+var y = 0;
+
+// Animação da publicidade
+function publicidade() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height) // Apaga o canvas
+
+
+    ctx.drawImage(imagem, x, y) //desenha a imagem
+    if (x > 0)                                                      // Se o x for maior que 0
+        ctx.drawImage(imagem, 1280 - x, 0, x, 720, 0, 0, x, 720)    // Desenhar o bocado da imagem (1290 - x, 0)
+                                                                    // Que esta fora do canvas
+    x++;                                                            // No ponto (0,0)
+    //console.log(x)
+    if (x == 1280)  //Quando o X chega ao fim da imagem
+        x = 0       //volta a ser 0
+
+}
+// Chamar a função da publicidade
+var timer = window.setInterval(publicidade, 1000 / 60)
+
 // Adiciona os quadrados ao array. Se for para mudar as coordenadas, muda-se aqui
 function criarQuadrado() {
     arrayQuadrados.push(new Quadrado(357, 81, 189, 97));
