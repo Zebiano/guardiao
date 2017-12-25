1a58e9572bc8c02a6d7c56ab53de04a399b370f6
Zebiano <sebas1@hotmail.de>
2017-12-25 15:15:54 +0000

    Adicionei comments

--- assets/common/js/config.js
+++ assets/common/js/config.js
@@ -1,6 +1,9 @@
+// Variaveis Globais
 var dificuldade;
 
+// Define a dificuldade
 function setDificuldade(e) {
+    // Para isto usamos uma forma de guardar data no browser, nem todos sao compativeis po isso fazse este check com o if (parecido cmo a prof faz o check do canvas)
     if (typeof (Storage) !== "undefined") {
         dificuldade = e;
 
@@ -11,13 +14,14 @@ function setDificuldade(e) {
         } else if (dificuldade == 3) {
             localStorage.setItem("lastname", "d");
         } else {
-            console.log("fuck you")
+            console.log("A dificuldade nao funcionou...")
         }
     } else {
         alert("Sorry, your browser does not support Web Storage... If you want to play this game you'll need a browser that supports it.");
     }
 }
 
+// Obtem a dificuldade
 function getDificuldade() {
     return localStorage.getItem("lastname");
 }
