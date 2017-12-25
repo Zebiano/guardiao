3daaba3fb51f556a119b52d00ae48b6cd4609c70
HugoBar <33893943+HugoBar@users.noreply.github.com>
2017-12-25 19:41:08 +0000

    assunto principal: animaçao da bola
    mudei algumas imagens e ajustei ao que precisavamos.
    
    No que toca a bola, tava a fazer na versao em que tava tudo na mesma file (animaçoes e resto do js) e o problema nesa versao e que so animava quando se fazia clique, ou seja, cada frame era equivalente a um clique. Deixei o codigo que fiz mas nao tentei por a funcionar pq ja tou um bocado cansado e tinha de pensar como e que funcionava o facto na escolha tar num ficheiro e a animaçao noutro.
    
    Adicionei um ficheiro excel com os pontos de cada curva para cada quadrado.
    
    Alguma duvida entrem em contacto

--- assets/common/js/animacoes.js
+++ assets/common/js/animacoes.js
@@ -1,7 +1,50 @@
 // Animacao da bola
-function animBola() {
-    // TODO
+var v = 0;
+var posX = 610; // variaçao da bola em X
+var posY = 583; // variaçao da bola em Y
+
+/* // Curva de Bezier
+function calc(v, x0, y0, x1, y1, x2, y2, x3, y3) {
+    posX = ((1 - v) * ((1 - v) * ((1 - v) * x0 + v * x1) + v * ((1 - v) * x1 + v * x2)) + v * ((1 - v) * ((1 - v) * x1 + v * x2) + v * ((1 - v) * x2 + v * x3)));
+    posY = (1 - v) * ((1 - v) * ((1 - v) * y0 + v * y1) + v * ((1 - v) * y1 + v * y2)) + v * ((1 - v) * ((1 - v) * y1 + v * y2) + v * ((1 - v) * y2 + v * y3));
+} */
+
+// Bola
+var imagem = new Image()
+imagem.src = '../assets/common/img/bola.png';
+
+// Desenho da Bola
+function desenhar(ponto1, ponto2, ponto3, ponto4, ponto5, ponto6, ponto7, ponto8) {
+
+    var x0 = ponto1;   // pontos da curva
+    var y0 = ponto2;
+    var x1 = ponto3;
+    var y1 = ponto4;
+    var x2 = ponto5;
+    var y2 = ponto6;
+    var x3 = ponto7;
+    var y3 = ponto8;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+    ctx.drawImage(imagem, posX, posY)
+
+    v += 0.01
+    posX = ((1 - v) * ((1 - v) * ((1 - v) * x0 + v * x1) + v * ((1 - v) * x1 + v * x2)) + v * ((1 - v) * ((1 - v) * x1 + v * x2) + v * ((1 - v) * x2 + v * x3)));
+    posY = (1 - v) * ((1 - v) * ((1 - v) * y0 + v * y1) + v * ((1 - v) * y1 + v * y2)) + v * ((1 - v) * ((1 - v) * y1 + v * y2) + v * ((1 - v) * y2 + v * y3));
+
+
+    if (v >= 1)
+        window.clearInterval(timerBola)
+
+   
 }
+  // Atribuir movimento da bola a cada escolha
+  if (escolha == 1) {
+    var timerBola = window.setInterval(desenhar(610, 583, 368, 450, 280, 186, 429, 110), 10)
+}
+
 
 // Animacao do guarda-redes
 function animRedes() {
@@ -12,6 +55,8 @@ function animRedes() {
 function animPublicidade() {
     var imagem = new Image() // Criar variavel imagem
     imagem.src = '../assets/common/img/placares.png'; // Atribuir imagem a variavel
+    var baliza = new Image() // Criar variavel imagem
+    baliza.src = '../assets/common/img/baliza.png'; // Atribuir imagem a variavel
 
     // Canto superior esquerdo - onde vamos desenhar a imagem
     var x = 0;
@@ -22,6 +67,7 @@ function animPublicidade() {
         ctx.clearRect(0, 0, canvas.width, canvas.height) // Apaga o canvas
 
         ctx.drawImage(imagem, x, y) //desenha a imagem
+
         if (x > 0) {                                                    // Se o x for maior que 0
             ctx.drawImage(imagem, 1280 - x, 0, x, 720, 0, 0, x, 720)    // Desenhar o bocado da imagem (1290 - x, 0) Que esta fora do canvas No ponto (0,0)
         }
@@ -30,6 +76,7 @@ function animPublicidade() {
         if (x == 1280) {  //Quando o X chega ao fim da imagem
             x = 0       //volta a ser 0
         }
+        ctx.drawImage(baliza, 0, 0) //desenha a imagem
     }
 
     // Chamar a função da publicidade
@@ -40,6 +87,8 @@ function animPublicidade() {
 function animPlateia() {
     // TO DO
 }
+
+
 
 //animBola();
 //animRedes();
